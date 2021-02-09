9a08da4801eb137a4a08c3aed6828c8fbe6625dd
wilmerbaby <wilmerbaby@gmail.com>
2021-02-09 18:51:40 -0500

    trouble shooting asigned variable from element by id resulting in object object when appending to it solution not assignig to a var and apending derictly

--- script.js
+++ script.js
@@ -4,11 +4,10 @@ var eyp = document.getElementById("endYear");
 var nRecords = document.getElementById("records");
 var searchButton = document.getElementById("sbutton");
 var deletebutton = document.getElementById("dbutton");
-var canva = document.getElementById("apme");
-var vwriter = document.getElementById("arth");
+// var canva = document.getElementById("apme");
 var llave = "t2H8QEgLqUNC08Pyh1327WAB5fjQEfxs";
 var rl = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
-console.log(canva);
+
 
 
 
@@ -19,23 +18,22 @@ $(document).ready(function(){
         url: "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=president&api-key="+ llave,
         method: "GET"
     }).then(function(res){
-        for(var i = 0)
-        console.log(res.response);
-        console.log(res.response.docs[0].headline.main);
-     var r1 = res.response.docs[0].headline.main
-     var writers = res.response.docs[0].byline.original
-     console.log(r1)
-     canva.append(r1)
-     vwriter.append(writers)
-
+   
+        var ress = res.response.docs
        
-        
+        for(var i = 0; i < ress.length; i++){
+            var esto = ress[i].headline.main;
+            var $lista = $("<ul>");
+            $lista.addClass("list-group");
+            $("#apme").append($lista)
+            $lista.append(esto)
+            // console.log(typeof lista)
+            // lista.append("<li>" + JSON.stringify(esto ) + "</li>")
+            
+            // canva.append("<li>" + JSON.stringify(esto ) + "</li>" + "<br>")
+             
+        }
+       
     })
-
-    // searchButton.addEventListener("submit", function(event){
-    //     event.preventDefault();
-
-     
-    // })
 
 })
